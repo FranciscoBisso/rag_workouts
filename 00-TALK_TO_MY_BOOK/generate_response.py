c71c9c2f8bdc9f1d517c2d0db03e70745a85075c
FranciscoBisso <fjbisso@gmail.com>
2024-10-09 14:57:34 -0300

    modified: .gitignore. Add generate_embeddings() in index_data.py. Create generate_response.py and add to it retrieve_related_docs()

---
+++ 00-TALK_TO_MY_BOOK/generate_response.py
@@ -0,0 +1,43 @@
+"""
+Module for handling queries and responses using Langchain and Chroma.
+"""
+
+import os
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from index_data import persistent_dir
+
+
+current_dir: str = os.getcwd()
+vector_store_path: str = persistent_dir
+question: str = "¿Qué elementos componen el ciclo procedimental?"  # <-- temporary
+
+
+def retrieve_related_docs(query: str):
+    """Retrieves relevant documents based on the query."""
+    embeddings_model = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
+    )
+    db = Chroma(
+        persist_directory=vector_store_path, embedding_function=embeddings_model
+    )
+
+    retriever = db.as_retriever(
+        search_type="similarity",
+        search_kwargs={"k": 5},
+        # search_type="mmr",
+        # search_kwargs={"k": 5, "fetch_k": 15, "lambda_mult": 0.5},
+        # search_type="similarity_score_threshold",
+        # search_kwargs={"k": 5, "score_threshold": 0.1}
+    )
+
+    related_docs = retriever.invoke(query)
+
+    return related_docs
+
+
+if __name__ == "__main__":
+    relevant_docs = retrieve_related_docs(question)
+    print("RELEVANT DOCUMENTS:\n")
+    for i, doc in enumerate(relevant_docs):
+        print(f"+ DOC {i}:\n{doc}\n\n{'==='*20}\n")
--- 00-TALK_TO_MY_BOOK/index_data.py
+++ 00-TALK_TO_MY_BOOK/index_data.py
@@ -4,11 +4,17 @@ Module for handling the indexing process of the markdown files.
 
 import os
 from typing import List
+from langchain_chroma import Chroma
 from langchain_core.documents import Document
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import MarkdownHeaderTextSplitter
 
+
 current_dir: str = os.getcwd()
-md_dir: str = os.path.join(os.getcwd(), "private_data")
+md_dir: str = os.path.join(current_dir, "private_data")
+persistent_dir: str = os.path.join(
+    current_dir, "database", "actuacion_del_abogado_en_la_causa_judicial"
+)
 
 
 def directory_loader(directory_path: str) -> List[Document]:
@@ -76,65 +82,94 @@ def split_by_headers(loaded_docs: Document) -> List[Document]:
     return docs_splitted_by_headers
 
 
-def split_by_paragraphs(
-    docs_splitted_by_headers: List[Document],
-) -> List[Document]:
+def split_by_paragraphs(docs_splitted_by_headers: List[Document]) -> List[Document]:
     """Splits based on paragraphs."""
-
     print("\n3. SPLITTING DOCUMENTS BY PARAGRAPHS...")
+
     docs_splitted_by_paragraphs: List[Document] = []
 
     for doc in docs_splitted_by_headers:
         paragraphs: List[str] = doc.page_content.split("\n")
 
         for paragraph in paragraphs:
-            final_doc = Document(
-                page_content=paragraph,
-                metadata=doc.metadata,
-            )
+            final_doc = Document(page_content=paragraph, metadata=doc.metadata)
             docs_splitted_by_paragraphs.append(final_doc)
 
     return docs_splitted_by_paragraphs
 
 
-def convert_headers(chunk_headers: dict) -> str:
+def stringify_headers(chunk_headers: dict) -> str:
     """Converts a dictionary to a string."""
-    
+
     formatted_headers: str = ""
-    
+
     for value in chunk_headers:
         formatted_headers += f"{chunk_headers[value]}\n"
-        
+
     return formatted_headers
 
-def add_headers_to_chunks_content(
-    list_of_docs: List[Document],
-) -> List[Document]:
-    """Add headers to the chunks content."""
 
-    for chunk in list_of_docs: 
+def move_headers_into_chunks_content(list_of_docs: List[Document]) -> List[Document]:
+    """Add headers to the chunks content."""
+    print("\n4. ENHANCING CHUNKS...")
+
+    docs_with_headers: List[Document] = []
+    for chunk in list_of_docs:
         headers = chunk.metadata["headers"]
-        chunk.page_content = f"{convert_headers(headers)}\n{chunk.page_content}"
-        
-    return list_of_docs
-        
+        content = f"{stringify_headers(headers)}\n{chunk.page_content}".strip()
+
+        del chunk.metadata["headers"]
+
+        full_doc = Document(metadata=chunk.metadata, page_content=content)
+        docs_with_headers.append(full_doc)
+
+    return docs_with_headers
+
+
+def chunks_lengths(chunks: List[Document]) -> str:
+    """Returns the size of each chunk."""
+
+    sizes = [len(chunk.page_content) for chunk in chunks]
+    unrepeated_lengths = sorted(set(sizes))
+    min_len = min(sizes)
+    max_len = max(sizes)
+
+    return f"- MIN: {min_len}\n+ MAX: {max_len}\n-> ALL: {unrepeated_lengths}"
+
+
+def generate_embeddings(chunks_to_embed: List[Document]) -> Chroma:
+    """Embeds document splits into the vector store."""
+    print("\n5. EMBEDDING CHUNKS...")
+
+    embeddings_model = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
+    )
+
+    vector_store = Chroma.from_documents(
+        documents=chunks_to_embed,
+        embedding=embeddings_model,
+        persist_directory=persistent_dir,
+    )
+
+    return vector_store
 
 
 if __name__ == "__main__":
-    loaded_docs = directory_loader(md_dir)
+    loaded_files = directory_loader(md_dir)
 
-    chunks_splitted_by_headers = split_by_headers(loaded_docs)
+    chunks_splitted_by_md_headers = split_by_headers(loaded_files)
 
-    chunks_splitted_by_paragraphs = split_by_paragraphs(chunks_splitted_by_headers)
-    
-    chunks_with_headers = add_headers_to_chunks_content(chunks_splitted_by_paragraphs)
+    chunks_splitted_by_paragraphs = split_by_paragraphs(chunks_splitted_by_md_headers)
 
-    # for i, document in enumerate(splitted_by_headers):
-    # for i, document in enumerate(splitted_by_paragraphs):
-    #     print(
-    #         f"""DOC N° {i}:\n\n- METADATA:\n{document.metadata["headers"]}\n\n- CONTENT:\n{document.page_content[:100]}\n\n{'==='*20}\n"""
-    #     )
-    for i, document in enumerate(chunks_with_headers):
-        print(
-            f"""DOC N° {i}:\n{document.page_content}\n\n{'==='*20}\n"""
-        )
+    chunks_with_headers = move_headers_into_chunks_content(
+        chunks_splitted_by_paragraphs
+    )
+
+    lengths = chunks_lengths(chunks_with_headers)
+
+    embeddings_generator = generate_embeddings(chunks_with_headers)
+
+    # for index, val in enumerate(chunks_with_headers):
+    #     print(f"""DOC N° {index}:\n{val.metadata}\n\n{'==='*20}\n""")
+
+    # print(f"LENGTHS:\n{lengths}")
