416d3d2618931f9aee27322254ba7cd95b8ad3df
FranciscoBisso <fjbiss@gmail.com>
2024-10-19 20:11:42 -0300

    private_data: minor splelling corrections to docs

--- 00-TALK_TO_MY_BOOK/generate_response.py
+++ 00-TALK_TO_MY_BOOK/generate_response.py
@@ -10,9 +10,10 @@ from index_data import persistent_dir, embeddings_model
 current_dir: str = os.getcwd()
 vector_store_path: str = persistent_dir
 # question: str = "¿Qué elementos componen el ciclo procedimental?"  # <-- temporary
-question: str = (
-    "¿Qué diferencia hay entre la instancia y la actuación simple?"  # <-- temporary
-)
+# question: str = (
+#     "¿Qué diferencia hay entre la instancia y la actuación simple?"  # <-- temporary
+# )
+question: str = "¿Qué se debe tener en cuenta a la hora de decidir si recurrir o no?"
 
 
 def retrieve_related_docs(query: str):
@@ -26,9 +27,9 @@ def retrieve_related_docs(query: str):
         search_type="similarity",
         search_kwargs={"k": 5},
         # search_type="mmr",
-        # search_kwargs={"k": 5, "fetch_k": 15, "lambda_mult": 0.5},
+        # search_kwargs={"k": 5, "fetch_k": 15, "lambda_mult": 0.7},
         # search_type="similarity_score_threshold",
-        # search_kwargs={"k": 5, "score_threshold": 0.1}
+        # search_kwargs={"k": 5, "score_threshold": 0.7}
     )
 
     related_docs = retriever.invoke(query)
@@ -38,7 +39,6 @@ def retrieve_related_docs(query: str):
 
 if __name__ == "__main__":
     relevant_docs = retrieve_related_docs(question)
-    print(f"DIR: {persistent_dir}")
-    print("RELEVANT DOCUMENTS:\n")
+    print(f"-> VECTOR STORE: {'/'.join(persistent_dir.split('/')[-2:])}\n\n")
     for i, doc in enumerate(relevant_docs):
-        print(f"DOC {i+1}:\n\n{doc.page_content}\n\n{'==='*20}\n")
+        print(f"DOC {i+1}:\n\n{doc.metadata['headers']}\n{doc.page_content}\n\n{'==='*20}\n")
