da52f2fd40f9c0aea08467a06beb1c8219346fe1
FranciscoBisso <fjbiss@gmail.com>
2024-12-25 21:57:27 -0300

    01-PROOFREADER: requirements.txt > Update | pdf_handler > index_docs()

--- 01-PROOFREADER/pdf_handler.py
+++ 01-PROOFREADER/pdf_handler.py
@@ -1,17 +1,22 @@
 """HANDLER FOR THE PDF FILES"""
 
-# from io import BytesIO
 import os
+import dotenv
 import tempfile
+from langchain_chroma import Chroma
 from langchain_core.documents import Document
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 from langchain_text_splitters import SentenceTransformersTokenTextSplitter
 from streamlit.runtime.uploaded_file_manager import UploadedFile
+from pydantic import SecretStr
 from typing import List
 
+dotenv.load_dotenv()
 
 current_dir: str = os.getcwd()
 hf_model: str = "intfloat/multilingual-e5-large"
+HF_API_KEY: SecretStr = SecretStr(os.getenv("HF_API_KEY"))
 
 
 def load_files(uploaded_bibliography: List[UploadedFile]) -> List[List[Document]]:
@@ -19,10 +24,11 @@ def load_files(uploaded_bibliography: List[UploadedFile]) -> List[List[Document]
     ARGS: uploaded_bibliography (List[UploadedFile])
     RETURNS: List[List[Document]]. EACH INNER LIST CORRESPONDS TO A PDF FILE
     """
-    print("LOADING PDF FILES...")
 
     if not uploaded_bibliography:
         raise ValueError("No se encontraron archivos PDF subidos")
+
+    print("LOADING PDF FILES...")
 
     all_documents = []
 
@@ -39,17 +45,13 @@ def load_files(uploaded_bibliography: List[UploadedFile]) -> List[List[Document]
             loader = PyPDFLoader(temp_path)
             documents = loader.load()
 
-            for i, doc in enumerate(documents):
+            for doc in documents:
                 doc.metadata["source"] = (
                     pdf_file.name.strip()
                     .upper()
                     .replace(".PDF", ".pdf")
                     .replace(". ", "_")
                 )
-                # if i < 10:
-                #     print(
-                #         f"\tDOC N° {i+1}\nLEN(CONTENT): {len(doc.page_content)}\n\n{'==='*15}\n"
-                #     )
 
             all_documents.append(documents)
 
@@ -62,14 +64,16 @@ def split_by_tokens(bibliography: List[List[Document]]) -> List[Document]:
     RETURNS: List[Document]. ALL THE DOCUMENTS SPLIT BY TOKENS INTO SMALLER CHUNKS
     """
 
-    print("SPLITTING BIBLIOGRAPHY BY TOKENS...")
-
     if not bibliography:
         raise ValueError("Ups! No se encontró bibliografía a ser dividida")
 
+    print("SPLITTING BIBLIOGRAPHY BY TOKENS...")
+
     documents: List[Document] = []
     splitter = SentenceTransformersTokenTextSplitter(
-        model_name=hf_model, tokens_per_chunk=500, chunk_overlap=12
+        model_name=hf_model,
+        tokens_per_chunk=500,
+        chunk_overlap=12,
     )
 
     for book in bibliography:
@@ -85,11 +89,12 @@ def split_by_tokens(bibliography: List[List[Document]]) -> List[Document]:
     return documents
 
 
-def count_tokens(documents: List[Document]):
+def count_tokens(documents: List[Document]) -> str:
     """
     ARGS: documents (List[Document])
     RETURNS: int. MIN & MAX NUMBER OF TOKENS IN THE DOCUMENTS
     """
+
     print("COUNTING TOKENS...")
 
     counter = SentenceTransformersTokenTextSplitter(model_name=hf_model)
@@ -98,4 +103,49 @@ def count_tokens(documents: List[Document]):
         counter.count_tokens(text=doc.page_content) for doc in documents
     ]
 
-    print(f"MIN: {min(tokens_per_doc)}\nMAX: {max(tokens_per_doc)}")
+    return f"\t - MIN: {min(tokens_per_doc)}\n\t - MAX: {max(tokens_per_doc)}"
+
+
+def index_docs(documents: List[Document]) -> Chroma:
+    """
+    GENERATES EMBEDDINGS FOR THE DOCUMENTS AND STORES THEM IN A TEMPORARY IN-MEMORY DATABASE.
+    """
+
+    if not documents:
+        raise ValueError("No se encontraron documentos para indexar.")
+
+    embeddings_model = HuggingFaceInferenceAPIEmbeddings(
+        api_key=HF_API_KEY,
+        model_name=hf_model,
+    )
+
+    # VERIFY API CONNECTION OF EMBEDDING'S MODEL
+    try:
+        embeddings_model.embed_query("Test query")
+        print("API connection successful")
+    except Exception as e:
+        print(f"API connection failed: {str(e)}")
+        raise
+
+    batch_size: int = 100
+    texts: List[str] = [doc.page_content for doc in documents]
+    metadatas: List[dict] = [doc.metadata for doc in documents]
+
+    # INITIALIZE CHROMA VECTOR STORE
+    chroma_vector_store = Chroma(
+        collection_name="bibliography",
+        embedding_function=embeddings_model,
+    )
+
+    print("INDEXING DOCS IN VECTOR STORE...")
+    # ADD BATCHES INTO CHROMA VECTOR STORE
+    for i in range(0, len(texts), batch_size):
+        batch_texts = texts[i : i + batch_size]
+        batch_metadata = metadatas[i : i + batch_size]
+        chroma_vector_store.add_texts(texts=batch_texts, metadatas=batch_metadata)
+        print(
+            f"\t- Processed batch {i//batch_size + 1}/{(len(texts) + batch_size - 1)//batch_size}"
+        )
+
+    print("DOCS SUCCESSFULLY INDEXED IN VECTOR STORE...")
+    return chroma_vector_store
--- 01-PROOFREADER/ui.py
+++ 01-PROOFREADER/ui.py
@@ -1,13 +1,19 @@
 """UI for the Proofreader app"""
 
 import os
+from langchain_chroma import Chroma
 from langchain_core.documents import Document
 import streamlit as st
 from streamlit.runtime.uploaded_file_manager import UploadedFile
 from typing import List
 
 # LOCAL IMPORTS
-from pdf_handler import load_files, split_by_tokens, count_tokens
+from pdf_handler import (
+    load_files,
+    split_by_tokens,
+    index_docs,
+    count_tokens,
+)
 
 current_dir: str = os.getcwd()
 
@@ -32,7 +38,18 @@ with st.sidebar:
     if uploaded_bibliography:
         bibliography: List[List[Document]] = load_files(uploaded_bibliography)
         documents: List[Document] = split_by_tokens(bibliography)
-        minimax_tokens: str = count_tokens(documents)
+        print(f"TOKENS:\n{count_tokens(documents)}")
+        vector_store: Chroma = index_docs(documents)
+        relevant_docs: List[Document] = vector_store.similarity_search(
+            query="Pasos del iter recursivo", k=10
+        )
+
+        if relevant_docs:
+            print("RETRIEVED DOCS:\n")
+            for i, doc in enumerate(relevant_docs):
+                print(
+                    f"DOC N°: {i+1}\nMETADATA:{doc.metadata}\n{doc.page_content}\n\n{'+++'*15}\n"
+                )
 
     # EXAMS UPLOADER
     uploaded_exams: list[UploadedFile] | None = st.file_uploader(
