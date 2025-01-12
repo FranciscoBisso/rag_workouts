00243c3f63b3f1bfb4a5fa00ab068760ca9b8804
FranciscoBisso <fjbiss@gmail.com>
2025-01-12 17:34:26 -0300

    00-TALK_TO_MY_BOOK: ui.py > change full_response type from str to list

--- 00-TALK_TO_MY_BOOK/ui.py
+++ 00-TALK_TO_MY_BOOK/ui.py
@@ -1,6 +1,7 @@
 """STREAMLIT'S UI"""
 
 import streamlit as st
+from typing import List
 from llm_handler import get_response_from_llm
 
 # PAGE'S CONFIG
@@ -45,16 +46,16 @@ if user_input := st.chat_input("¿En qué puedo ayudarte?"):
     # PLACEHOLDER FOR ASSISTANT'S RESPONSE
     with st.chat_message("assistant", avatar="🧑‍⚖️"):
         response_placeholder = st.empty()
-        full_response: str = ""
+        full_response: List = []
 
         # SHOW SPINNER WHILE STREAMING RESPONSE
         with st.spinner(":gray[_Generando respuesta..._]"):
             for chunk in get_response_from_llm(user_input):
                 # ACCUMULATE STREAMED CHUNKS
-                full_response += chunk.content
-                response_placeholder.markdown(full_response)
+                full_response.append(str(chunk.content))
+                response_placeholder.markdown("".join(full_response))
 
         # ADD ASSISTANT'S RESPONSE TO CHAT HISTORY
         st.session_state.messages.append(
-            {"role": "assistant", "content": full_response}
+            {"role": "assistant", "content": "".join(full_response)}
         )
